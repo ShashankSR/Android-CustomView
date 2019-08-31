c2805c50571e366b350f07d56835281b44204dbb
ShashankSR <shashanksheshan@gmail.com>
2019-08-31 13:16:59 +0530

    Add data binding to activity

--- app/src/main/java/com/example/credit/MainActivity.kt
+++ app/src/main/java/com/example/credit/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.credit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.credit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    val creditCardViewModel = CreditCardViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this, R.layout.activity_main
+        ).apply {
+            credViewModel = creditCardViewModel
+            lifecycleOwner = this@MainActivity
+        }
     }
 }
