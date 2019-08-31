0fc7e3d41ad8e135e3ea1936c1fbe23c256e7d3b
ShashankSR <shashanksheshan@gmail.com>
2019-08-31 12:16:50 +0530

    Add card type checks algorithm

--- app/src/main/java/com/example/credit/CreditCardViewModel.kt
+++ app/src/main/java/com/example/credit/CreditCardViewModel.kt
@@ -2,6 +2,7 @@ package com.example.credit
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.regex.Pattern
 
 class CreditCardViewModel : ViewModel() {
 
@@ -31,7 +32,39 @@ class CreditCardViewModel : ViewModel() {
         return sum % 10 == 0
     }
 
-    fun updateCardType() {
+    enum class CardType {
+
+        UNKNOWN,
+        VISA("^4[0-9]{12}(?:[0-9]{3}){0,2}$"),
+        MASTERCARD("^(?:5[1-5]|2(?!2([01]|20)|7(2[1-9]|3))[2-7])\\d{14}$"),
+        AMERICAN_EXPRESS("^3[47][0-9]{13}$"),
+        DINERS_CLUB("^3(?:0[0-5]\\d|095|6\\d{0,2}|[89]\\d{2})\\d{12,15}$"),
+        DISCOVER("^6(?:011|[45][0-9]{2})[0-9]{12}$"),
+        JCB("^(?:2131|1800|35\\d{3})\\d{11}$"),
+        CHINA_UNION_PAY("^62[0-9]{14,17}$");
+
+        private var pattern: Pattern? = null
+
+        constructor() {
+            this.pattern = null
+        }
+
+        constructor(pattern: String) {
+            this.pattern = Pattern.compile(pattern)
+        }
+
+        companion object {
+
+            fun detect(cardNumber: String): CardType {
+
+                for (cardType in values()) {
+                    if (null == cardType.pattern) continue
+                    if (cardType.pattern!!.matcher(cardNumber).matches()) return cardType
+                }
+
+                return UNKNOWN
+            }
+        }
 
     }
 }
